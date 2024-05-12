285c653082ef33b7bf3ed5b530baec4593299612
Khip01 <aakhifathallah24@gmail.com>
2024-05-12 10:27:03 +0700

    added new kata solution

---
+++ 14.KeepHidrated.dart
@@ -0,0 +1,10 @@
+/**
+ *  Title: Keep Hidarated [8 kyu]
+ *  link: https://www.codewars.com/kata/582cb0224e56e068d800003c/
+ */
+
+void main() {
+  print(litres(11.8));
+}
+
+int litres(num time) => (time / 2).floor();
